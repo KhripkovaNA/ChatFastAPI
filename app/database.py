53e79a203c9a82921fbc3c4cb89b7098c97a7540
khripna <khripna@gmail.com>
2024-10-19 20:10:16 +0300

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DB_HOST=localhost
+DB_PORT=5432
+DB_NAME=postgres
+DB_USER=postgres
+DB_PASS=Russia_will_be_4ree
+
+
