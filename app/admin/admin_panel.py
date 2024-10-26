bf9235a48caf12fba027872a887b529f40f8563c
khripna <khripna@gmail.com>
2024-10-27 00:19:52 +0300

    readme and admin edit

--- app/admin/admin_panel.py
+++ app/admin/admin_panel.py
@@ -11,7 +11,7 @@ class UsersAdmin(ModelView, model=Users):
     name_plural = "Users"
     icon = "fa-solid fa-user"
     # Поля, которые будут отображаться в списке пользователей
-    column_list = [Users.id, Users.name, Users.email, Users.role]
+    column_list = [Users.id, Users.name, Users.email, Users.role, Users.tg_id]
     # Исключить отображение поля с паролем в деталях пользователя
     column_details_exclude_list = [Users.hashed_password]
     # Отображение поля hashed_password как password
@@ -19,8 +19,8 @@ class UsersAdmin(ModelView, model=Users):
     # Отключить возможность удаления пользователей
     can_delete = False
     # Правила для создания и редактирования пользователей
-    form_create_rules = ["name", "email", "hashed_password", "role"]
-    form_edit_rules = ["name", "email", "role"]
+    form_create_rules = ["name", "email", "hashed_password", "role", "tg_id"]
+    form_edit_rules = ["name", "email", "role", "tg_id"]
 
     async def on_model_change(self, data, model, is_created, request) -> None:
         # Хеширование пароля при создании нового пользователя
@@ -34,8 +34,9 @@ class MessagesAdmin(ModelView, model=Messages):
     icon = "fa-solid fa-message"
     # Поля, которые будут отображаться в списке сообщений
     column_list = [Messages.id, Messages.sender, Messages.recipient, Messages.content]
-    # Отключить возможность добавление сообщений
+    # Отключить возможность добавления сообщений
     can_create = False
+    # Правила для редактирования сообщений
     form_edit_rules = ["content"]
 
 
